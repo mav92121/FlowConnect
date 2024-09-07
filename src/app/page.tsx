8e1893cba725d2810eb78e2003dd1424b8dc751d
Mohsinali Vijapura <mohsinali530121@gmail.com>
2024-09-07 14:41:55 +0530

    user avatar added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,9 @@
 "use client";
-import { Button } from "@/components/ui/button";
-import { useAuthActions } from "@convex-dev/auth/react";
-import React from "react";
-
+import UserButton from "@/features/auth/components/user-button";
 const Home = () => {
-  const { signOut } = useAuthActions();
   return (
     <div>
-      Logged In
-      <div>
-        <Button onClick={() => signOut()}>Sign Out</Button>
-      </div>
+      <UserButton />
     </div>
   );
 };
